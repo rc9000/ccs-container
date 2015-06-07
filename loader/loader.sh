e99a77df53dca7dfbe979ee947d440815415ed8f
Christian Ramseyer <christian.ramseyer@netnea.com>
2015-06-07 20:41:25 +0200

    fixed environment for loader.sh

--- loader/loader.sh
+++ loader/loader.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export CCS=/opt/ccs
+
 # delete all documents and load the newly generated xml files
 CORE_URL=http://localhost:8983/solr/configsearchcore
 curl $CORE_URL/update --data '<delete><query>*:*</query></delete>' -H 'Content-type:text/xml; charset=utf-8'
