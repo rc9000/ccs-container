c07cd69a5a4c3c83f3a174f7c0e9ba3e14bcefd0
Christian Ramseyer <christian.ramseyer@netnea.com>
2015-08-23 17:31:59 +0200

    testing content_trigram tweaking for 'vlan 20' and similar

--- frisby-tests/spec/test_spec.js
+++ frisby-tests/spec/test_spec.js
@@ -22,6 +22,16 @@ frisby.create('Find substring description in some docs')
   })
 .toss();
 
+frisby.create('Find substring vlan 20 in core2')
+  .get(coreUrl + '/select?q=content%3Avlan+20&fq=doctype%3A%22full+config%22&wt=json&indent=true&debugQuery=true&defType=edismax&mm=100%25')
+  .expectStatus(200)
+  .expectJSON({
+    response: {
+      numFound: 1
+    }
+  })
+.toss();
+
 
 
 
